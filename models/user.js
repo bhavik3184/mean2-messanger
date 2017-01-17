b01c950cef8e95e488662217f09d08a48c4946f5
bhavik3184 <bhavik3184@gmail.com>
2017-01-17 12:46:40 +0530

    MongoDB Models (Server Side) - Created new models i.e. user and messages along with a new hbs view which contains a form to save data to mongodb & retrieve the same.

---
+++ models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongooseUniqueValidator = require('mongoose-unique-validator');
+
+var schema = new Schema({
+    firstname: {type: String, required: true}
+    , lastname: {type: String, required: true}
+    , email: {type: String, required: true, unique: true}
+    , messages: {type: Schema.Types.ObjectId, ref: 'Messages'}
+});
+
+schema.plugin(mongooseUniqueValidator);
+module.exports = mongoose.model('User', schema);
