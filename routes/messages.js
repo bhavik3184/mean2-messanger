fb4be00bff74bd5c4328dc0aa077408654be3248
bhavik3184 <bhavik3184@gmail.com>
2017-01-30 10:42:54 +0530

    Backend service - setting up post service on server for saving messages

--- routes/messages.js
+++ routes/messages.js
@@ -3,17 +3,17 @@ var router = express.Router();
 var Message = require('../models/message');
 
 router.post('/', function (req, res, next) {
-    var message = new message({
+    var message = new Message({
         content: req.body.content
     });
     message.save(function (err, result) {
         if (err) {
-            return res.statusCode(500).json({
+            return res.status(500).json({
                 title: 'An error occurred'
                 , obj: err
             })
         }
-        res.statusCode(201).json({
+        res.status(201).json({
             message: 'Saved message'
             , obj: result
         })
