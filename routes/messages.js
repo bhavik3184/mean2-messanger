b7a52e098b26a3da0c3a7a803d1146e9b8dd1a53
bhavik3184 <bhavik3184@gmail.com>
2017-02-01 20:52:47 +0530

    updating message - setting up backend

--- routes/messages.js
+++ routes/messages.js
@@ -9,13 +9,13 @@ router.get('/', function (req, res, next) {
                 return res.status(500).json({
                     title: 'An error occoured'
                     , obj: err
-                })
+                });
             }
             res.status(201).json({
                 message: 'Success'
                 , obj: messages
-            })
-        })
+            });
+        });
 
 });
 
@@ -33,6 +33,36 @@ router.post('/', function (req, res, next) {
         res.status(201).json({
             message: 'Saved message'
             , obj: result
+        });
+    });
+});
+
+router.patch('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function (err, message) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occoured'
+                , obj: err
+            })
+        }
+        if (!message) {
+            return res.status(500).json({
+                title: 'An error occoured'
+                , error: {message: 'Message not found'}
+            })
+        }
+        message.content = req.body.content;
+        message.save(function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occoured'
+                    , obj: err
+                })
+            }
+            res.status(200).json({
+                message: 'Message updated succesfully'
+                , obj: result
+            })
         })
     });
 });
