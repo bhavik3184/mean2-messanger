ae41c4342cdd8126a71eb1add8c27d2982dc7d16
bhavik3184 <bhavik3184@gmail.com>
2017-02-06 15:53:23 +0530

    added password field to User model - which will be used to store passwords in DB

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ var schema = new Schema({
     firstName: {type: String, required: true}
     , lastName: {type: String, required: true}
     , email: {type: String, required: true, unique: true}
+    , password: {type: String, required: true}
     , messages: {type: Schema.Types.ObjectId, ref: 'Messages'}
 });
 
